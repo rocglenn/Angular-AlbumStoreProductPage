8269fce3be1845c346ed99e8f695b216278a5f88
rocglenn <roocglenn@gmail.com>
2021-08-29 20:25:53 -0500

    changed the import of the service

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -8,11 +8,13 @@ import { ProductService } from 'app/product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo: any;
+  
   constructor(private _productService:ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => {
       this.albumInfo = response;
+      //console.log(response);
   });    
   }
 
